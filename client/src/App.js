e2e11ab02dc2b39cd81d6e0300c8bd8482ed8076
X13QQ <73265917+X13QQ@users.noreply.github.com>
2020-12-31 16:08:05 +0800

    add

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,6 @@ function App() {
       <div className="App">Main1</div>
       <div className="App">Main2</div>
       <div className="App">Main3</div>
-      <div className="App">Main4</div>
-      <div className="App">Main5</div>
-      <div className="App">Main6</div>
-      <div className="App">Main7</div>
-      <div className="App">Main8</div>
-      <div className="App">Main9</div>
-      <div className="App">Main1111</div>
     </>
   )
 }
