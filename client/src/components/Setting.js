6a0125426161aee05d54f24194fb6e4edd1174b8
X13QQ <73265917+X13QQ@users.noreply.github.com>
2021-01-11 16:48:43 +0800

    0111 add

---
+++ client/src/components/Setting.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Setting = (props) => {
+  return (
+    <>
+      <div>設定</div>
+    </>
+  )
+}
+
+export default Setting
---
+++ client/src/pages/MainSetting.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Setting from '../components/Setting'
+
+function MainRefund(props) {
+  return (
+    <>
+      <div className="main col-9">
+        <div className="title ml-3 my-2">退款申請</div>
+        <div className="mx-2 border border-top-0">
+          <Setting />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MainRefund
