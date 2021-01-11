b728f20d5d792584643c83cd90bddb8dc508afb5
X13QQ <x13qqjax@gmail.com>
2021-01-12 00:19:55 +0800

    0112jaxadd

--- server/app.js
+++ server/app.js
@@ -1,7 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql");
 
+const db = mysql.createPool({
+  host: "localhost",
+  user: "cool",
+  password: "12345",
+  database: "cool",
+});
 
-app.listen(3000, () => {
-    console.log('port 3000')
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/member/refund/:status", function (req, res) {
+  //   console.log(req.params.status);
+  let status = "";
+  if ("processing" === req.params.status) status = 1;
+  if ("solved" === req.params.status) status = 2;
+  if ("refundlist" === req.params.status) status = 3;
+  const sqlSelect = "SELECT * FROM cool_order WHERE status = ? ";
+  db.query(sqlSelect, [status], (err, result, fields) => {
+    res.send(result);
+  });
+});
+
+app.listen(3001, () => {
+  console.log("port 3001");
+});
