1b3786669c0f7b722676c3852a892fcd1a59a42c
pinciao <71361843+sakulahebi@users.noreply.github.com>
2021-01-11 17:21:20 +0800

    0111_pinciao

--- client/src/pages/MainSetting.js
+++ client/src/pages/MainSetting.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import Setting from '../components/Setting'
+// import Setting from '../components/Setting'
 
-function MainRefund(props) {
+function MainSetting(props) {
   return (
     <>
       <div className="main col-9">
-        <div className="title ml-3 my-2">退款申請</div>
-        <div className="mx-2 border border-top-0">
-          <Setting />
-        </div>
+        <div className="title ml-3 my-2">帳號設定!!!!</div>
+        <div className="mx-2 border border-top-0"></div>
       </div>
     </>
   )
 }
 
-export default MainRefund
+export default MainSetting
